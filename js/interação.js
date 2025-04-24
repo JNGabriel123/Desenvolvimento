d83dc12c416aa6e0361b66353c6c139dc55b1138
JNgabriel <gabrielcacelesbao26@gmail.com>
2025-04-24 17:31:06 -0400

    Algumas alterações no responsivo para mobile

--- js/interação.js
+++ js/interação.js
@@ -1,20 +1,9 @@
-// let A = 10;
-// let B = 9;
-
-// let result = A + B;
-
-// console.log("X = " + result);
-
-// A = 2, B = 3, C = 2, D = 6
-
-
-
 function toggleContent() {
-    var content = document.getElementById("hiddenContent");
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
+    var content = document.getElementById('hiddenContent');
+    if (content.style.display === 'none' || content.style.display === "") {
+        content.style.display = 'block';
     } else {
-        content.style.display = "none";
+        content.style.display = 'none';
     }
 }
 
