aa3cce8be6128fe0fdd1daed4f7e817b42c50a49
JNgabriel <gabrielcacelesbao26@gmail.com>
2025-06-05 15:42:06 -0400

    Mudança de desinig dos modulos e alerta quando sobe de nivel

---
+++ formatacao/alert.js
@@ -0,0 +1,9 @@
+Swal.fire({
+  title: '🎉 Parabéns!',
+  text: 'Você subiu para o nível 4',
+  icon: 'success',
+  confirmButtonText: 'Show!',
+  background: '#1f1f1f',
+  color: '#e1e1e1',
+  confirmButtonColor: '#00bcd4'
+});
--- js/points.js
+++ js/points.js
@@ -2,25 +2,27 @@ let currentPoints = 0;
 let level = 1;
 let levelThreshold = 10;
 
-const ctx = document.getElementById('pointsChart').getContext('2d');
+const ctx = document.getElementById("pointsChart").getContext("2d");
 const chart = new Chart(ctx, {
-  type: 'doughnut',
+  type: "doughnut",
   data: {
-    labels: ['Pontos', 'Faltam'],
-    datasets: [{
-      data: [0, levelThreshold],
-      backgroundColor: ['#00c853', '#eeeeee'],
-      borderWidth: 1,
-    }]
+    labels: ["Pontos", "Faltam"],
+    datasets: [
+      {
+        data: [0, levelThreshold],
+        backgroundColor: ["#229bff", "#becad4"],
+        borderWidth: 0,
+      },
+    ],
   },
   options: {
-    cutout: '70%',
+    cutout: "0%",
     responsive: true,
     plugins: {
       tooltip: { enabled: false },
-      legend: { display: false }
-    }
-  }
+      legend: { display: false },
+    },
+  },
 });
 
 function addPoints(p) {
@@ -29,7 +31,16 @@ function addPoints(p) {
     level++;
     currentPoints = currentPoints - levelThreshold;
     levelThreshold += 5; // pode ajustar o quanto cresce a dificuldade
-    alert(`🎉 Parabéns! Você subiu para o nível ${level}`);
+    // alert(`🎉 Parabéns! Você subiu para o nível ${level}`);
+    Swal.fire({
+      title: "🎉 Parabéns!",
+      text: "Você subiu para o nível " + level,
+      icon: "success",
+      confirmButtonText: "Show!",
+      background: "#1f1f1f",
+      color: "#e1e1e1",
+      confirmButtonColor: "#00bcd4",
+    });
   }
   updateChart();
 }
@@ -38,6 +49,9 @@ function updateChart() {
   chart.data.datasets[0].data = [currentPoints, levelThreshold - currentPoints];
   chart.update();
 
-  document.getElementById('scoreText').textContent =
-    `Você tem ${currentPoints} pontos. Faltam ${levelThreshold - currentPoints} para subir ao nível ${level + 1}`;
+  document.getElementById(
+    "scoreText"
+  ).textContent = `Você tem ${currentPoints} pontos. Faltam ${
+    levelThreshold - currentPoints
+  } para subir ao nível ${level + 1}`;
 }
